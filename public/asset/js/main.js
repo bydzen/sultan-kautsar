9ff19c8401c210ff21dbfb6add67a675b1b0ecad
bydzen <svnteentthones@gmail.com>
2025-03-04 11:17:56 +0700

    update icons 🎨

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,16 +1,18 @@
 function updateGreeting() {
     const hours = new Date().getHours();
     let greeting;
-
+    let emoji;
     if (hours >= 5 && hours < 12) {
         greeting = "good morning!";
+        emoji = "🌄"
     } else if (hours >= 12 && hours < 18) {
         greeting = "good afternoon!";
+        emoji = "⛅"
     } else {
         greeting = "good evening!";
+        emoji = "🌆";
     }
-
     document.getElementById("js-greeting").textContent = greeting;
+    document.getElementById("js-emoji").textContent = emoji;
 }
-
 updateGreeting();
--- wrangler-deploy.sh
+++ wrangler-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf "Deploying public/ to [sultan-kautsar] Cloudflare pages project...\n"
-wrangler pages deploy public --project-name sultan-kautsar
+printf "Deploying public/ to [sultankautsar] Cloudflare pages project...\n"
+wrangler pages deploy public --project-name sultankautsar
